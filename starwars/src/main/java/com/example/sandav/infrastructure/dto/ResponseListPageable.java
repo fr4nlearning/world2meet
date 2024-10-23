8dafb21bbc6165f42ea57863578d9d05fcf70e74
Fran <francis.gestion@gmail.com>
2024-10-23 03:25:15 +0200

    creacion de un objeto para el getall pageable

---
+++ starwars/src/main/java/com/example/sandav/infrastructure/dto/ResponseListPageable.java
@@ -0,0 +1,30 @@
+package com.example.sandav.infrastructure.dto;
+
+import com.example.sandav.domain.model.Starship;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ResponseListPageable<T> {
+
+    private List<T> starshipList;
+    private Long totalElements;
+    private Integer totalPages;
+    private Integer number;
+    private Integer size;
+
+    public ResponseListPageable(List<T> list, long totalElements, int page, int number, int size) {
+        this.starshipList= list;
+        this.totalElements= totalElements;
+        this.totalPages= page;
+        this.number= number;
+        this.size= size;
+    }
+}
