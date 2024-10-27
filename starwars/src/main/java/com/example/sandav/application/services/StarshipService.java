38681ff1e6ab0624d219f71a707ff84d910ddb3b
Fran <francis.gestion@gmail.com>
2024-10-27 15:30:56 +0100

    feat(cache): en todos los metodos del servicio

--- starwars/src/main/java/com/example/sandav/application/services/StarshipService.java
+++ starwars/src/main/java/com/example/sandav/application/services/StarshipService.java
@@ -4,6 +4,7 @@ import com.example.sandav.domain.model.Starship;
 import com.example.sandav.domain.port.IStarshipRepository;
 import com.example.sandav.infrastructure.dto.ResponseListPageable;
 import lombok.AllArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Pageable;
 
@@ -13,6 +14,7 @@ import java.util.Objects;
 public class StarshipService {
     private final IStarshipRepository iStarshipRepository;
 
+    @CacheEvict(value = "starships", key = "#starship.id")
     public Starship save(Starship starship){
         return this.iStarshipRepository.save(starship);
     }
@@ -22,10 +24,12 @@ public class StarshipService {
         return this.iStarshipRepository.findByNamePageable(name, pageable);
     }
 
+    @Cacheable(value = "starships", key= "#id")
     public Starship findById(Integer id){
         return this.iStarshipRepository.findById(id);
     }
 
+    @CacheEvict(value = "starships", key = "#id")
     public Starship update(Integer id, Starship starship){
 
         var ssRespository= this.iStarshipRepository.findById(id);
@@ -39,6 +43,7 @@ public class StarshipService {
                                 .build());
     }
 
+    @CacheEvict(value = "starships", key = "#id")
     public void deleteById(Integer id){
         this.iStarshipRepository.deleteById(id);
     }
