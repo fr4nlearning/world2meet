846e23c2d3fab8a8453861cb790a75b3f7ab7229
Fran <francis.gestion@gmail.com>
2024-11-02 14:13:59 +0100

    refactor(swagger): nuevo tag en los distintos controladores

--- starwars/src/main/java/com/example/sandav/infrastructure/rest/ILoginController.java
+++ starwars/src/main/java/com/example/sandav/infrastructure/rest/ILoginController.java
@@ -6,12 +6,14 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/api/v1/user")
+@Tag(name = "Login API", description = "API to manage user login")
 public interface ILoginController {
     @Operation(
             summary = "Login",
--- starwars/src/main/java/com/example/sandav/infrastructure/rest/IUserController.java
+++ starwars/src/main/java/com/example/sandav/infrastructure/rest/IUserController.java
@@ -5,12 +5,14 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/api/v1/user")
+@Tag(name = "User registration API", description = "API to register new users in the system")
 public interface IUserController {
     @Operation(
             summary = "Create a new user",
