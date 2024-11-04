8ea18e3c21105e4e9f738b65970a9780c7e21dcd
Fran <francis.gestion@gmail.com>
2024-11-04 01:30:46 +0100

    refactor(exception): he quitado una excepcion que no uso

--- starwars/src/main/java/com/example/sandav/infrastructure/exception/StarshipNotFoundException.java
+++ starwars/src/main/java/com/example/sandav/infrastructure/exception/StarshipNotFoundException.java
@@ -4,9 +4,4 @@ public class StarshipNotFoundException extends RuntimeException {
     public StarshipNotFoundException(Integer id) {
         super("There is not a starship with id: " + id);
     }
-
-    public StarshipNotFoundException(String name) {
-        super("There is not a starship that contains: " + name);
-    }
-
 }
