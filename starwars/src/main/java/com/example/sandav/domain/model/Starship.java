e00ca05383a8b339fcf13f1656d13eab661796f8
Fran <francis.gestion@gmail.com>
2024-11-09 01:29:49 +0100

    Revert "Cambios pequeños que aportan, validaciones"
    This reverts commit 358758c8975a58113b7eb1fba2a9bd349b65395a.

--- starwars/src/main/java/com/example/sandav/domain/model/Starship.java
+++ starwars/src/main/java/com/example/sandav/domain/model/Starship.java
@@ -1,6 +1,4 @@
 package com.example.sandav.domain.model;
 
-import jakarta.validation.constraints.NotNull;
-
-public record Starship(Integer id, @NotNull String name, @NotNull Faction faction) {
+public record Starship(Integer id, String name, Faction faction) {
 }
